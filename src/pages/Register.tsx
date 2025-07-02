067e10bd31ae54aef476a3b0206528d61acc74e8
Lutfiyadi Afandi <lutfiyadiafandi@gmail.com>
2025-07-02 19:23:18 +0800

    feat: create register page

---
+++ src/pages/Register.tsx
@@ -0,0 +1,11 @@
+import RegisterForm from "@/components/templates/RegisterForm";
+
+const Register = () => {
+  return (
+    <>
+      <RegisterForm />
+    </>
+  );
+};
+
+export default Register;
